6d599e94a61681f4a98b9625959e6b35765eafd0
Johannes <kabeleced@arcor.de>
2016-04-17 22:32:49 +0200

    added bower and split gruntfile to build firefox and chrome output

--- gruntfile.js
+++ gruntfile.js
@@ -1,41 +1,115 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+    var outDirectoryFirefox = "app-firefox";
+    var outDirectoryChrome = "app-chrome";
+
     grunt.initConfig({
+        cleanempty: {
+            options: {
+                // Task-specific options go here. 
+            },
+            chrome: {
+                src: [outDirectoryChrome + "/*"]
+            },
+            firefox: {
+                src: [outDirectoryFirefox + "/*"]
+            },
+        },
         ts: {
-            default: {
-                src: ["src/**/*.ts"],  //, "!node_modules/**/*.ts", "!typings/**/*.ts"]
-                outDir: ["app"],
-                options: {
-                    "target": "es5",
-                    "noImplicitAny": true,
-                    "removeComments": true,
-                    "preserveConstEnums": true,
-                    "outDir": "app",
-                    "sourceMap": true
-                }
+            options: {
+                "target": "es6",
+                "module": "es6",
+                "noImplicitAny": true,
+                "removeComments": true,
+                "preserveConstEnums": true,
+                "sourceMap": true
+            },
+            firefox: {
+                src: ["src/**/*.ts", "!src/UI/Settings/**/*.ts", "!node_modules/**/*.ts", "!typings/**/*.ts"],
+                outDir: [outDirectoryFirefox]
+            },
+            chrome: {
+                src: ["src/**/*.ts", "!src/UI/Settings/**/*.ts", "!node_modules/**/*.ts", "!typings/**/*.ts"],
+                outDir: [outDirectoryChrome]
             }
         },
         "merge-json": {
-            "i18n": {
-                files: {
-                    "app/_locales/de/messages.json": ["src/**/*-de.json"],
-                    "app/_locales/en/messages.json": ["src/**/*-en.json"]
-                }
+            "en-firefox": {
+                src: ["src/**/*-en.json"],
+                dest: outDirectoryFirefox + "/_locales/en/messages.json"
+            },
+            "de-firefox": {
+                src: ["src/**/*-de.json"],
+                dest: outDirectoryFirefox + "/_locales/de/messages.json"
+            },
+            "en-chrome": {
+                src: ["src/**/*-en.json"],
+                dest: outDirectoryChrome + "/_locales/en/messages.json"
+            },
+            "de-chrome": {
+                src: ["src/**/*-de.json"],
+                dest: outDirectoryChrome + "/_locales/de/messages.json"
             },
             "manifest-chrome": {
-                files: {
-                    "app/manifest.json": ["configurations/manifest-base.json"]
-                }
+                src: ["configurations/manifest-base.json"],
+                dest: outDirectoryChrome + "/manifest.json"
             },
             "manifest-firefox": {
-                files: {
-                    "app/manifest.json": ["configurations/manifest-base.json", "configurations/manifest-firefox.json"]
-                }
+                src: ["configurations/manifest-base.json", "configurations/manifest-firefox.json"],
+                dest: outDirectoryFirefox + "/manifest.json"
             }
         },
         processhtml: {
-            dist: {
+            chrome: {
+                src: ['src/UI/Settings/index.html'],
+                dest: outDirectoryChrome + '/UI/Settings/index.html'
+            },
+            firefox: {
+                src: ['src/UI/Settings/index.html'],
+                dest: outDirectoryFirefox + '/UI/Settings/index.html'
+            }
+        },
+        bowercopy: {
+            options: {
+                srcPrefix: 'bower_components'
+            },
+            "scripts-chrome": {
+                options: {
+                    destPrefix: outDirectoryChrome + '/scripts'
+                },
                 files: {
-                    'app/UI/Settings/options.html': ['src/UI/Settings/options.html']
+                    'angular.js': 'angular/angular.js',
+                    'angular-route.js': 'angular-route/angular-route.js',
+                    'jquery.js': 'jquery/dist/jquery.js',
+                    'require.js': 'requirejs/require.js'
+                }
+            },
+            "styles-chrome": {
+                options: {
+                    destPrefix: outDirectoryChrome + '/styles'
+                },
+                files: {
+                    'semantic.css': 'semantic/dist/semantic.css',
+                    'bootstrap.min.css': 'bootstrap/dist/css/bootstrap.min.css'
+                }
+            },
+            "scripts-firefox": {
+                options: {
+                    destPrefix: outDirectoryFirefox + '/scripts'
+                },
+                files: {
+                    'angular.js': 'angular/angular.js',
+                    'angular-route.js': 'angular-route/angular-route.js',
+                    'jquery.js': 'jquery/dist/jquery.js',
+                    'require.js': 'requirejs/require.js'
+                }
+            },
+            "styles-firefox": {
+                options: {
+                    destPrefix: outDirectoryFirefox + '/styles'
+                },
+                files: {
+                    'semantic.css': 'semantic/dist/semantic.css',
+                    'bootstrap.min.css': 'bootstrap/dist/css/bootstrap.min.css'
                 }
             }
         }
@@ -43,7 +117,28 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-ts");
     grunt.loadNpmTasks("grunt-merge-json");
     grunt.loadNpmTasks("grunt-processhtml");
+    grunt.loadNpmTasks('grunt-bowercopy');
+    grunt.loadNpmTasks('grunt-cleanempty');
 
-    grunt.registerTask("chrome", ["ts", "merge-json:i18n", "merge-json:manifest-chrome", "processhtml"]);
-    grunt.registerTask("firefox", ["ts", "merge-json:i18n", "merge-json:manifest-firefox", "processhtml"]);
+    grunt.registerTask("chrome", [
+        "ts:chrome",
+        "cleanempty:chrome",
+        "merge-json:en-chrome",
+        "merge-json:de-chrome",
+        "merge-json:manifest-chrome",
+        "processhtml:chrome",
+        "bowercopy:scripts-chrome",
+        "bowercopy:styles-chrome"]);
+    grunt.registerTask("firefox", [
+        "ts:firefox",
+        "cleanempty:firefox",
+        "merge-json:en-firefox",
+        "merge-json:de-firefox",
+        "merge-json:manifest-firefox",
+        "processhtml:firefox",
+        "bowercopy:scripts-firefox",
+        "bowercopy:styles-firefox"]);
+    grunt.registerTask("default", [
+        "chrome",
+        "firefox"]);
 };
