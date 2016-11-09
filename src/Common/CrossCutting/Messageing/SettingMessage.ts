a99bbcdc3504737ab962dc52ae34dd95b32817be
Johannes <Johannes@JO>
2016-11-09 20:34:18 +0100

    Removed SettingMessage

--- src/Common/CrossCutting/Messageing/SettingMessage.ts
+++
@@ -1,9 +0,0 @@
-import {AppSettings} from "../Settings/AppSettings"
-
-export class SettingMessage {
-	public settingKey: string;
-	public settingAction: SettingAction;
-	public appSettings: AppSettings;
-}	
-
-export enum SettingAction { Load, Save };
