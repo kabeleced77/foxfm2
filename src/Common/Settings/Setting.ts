12f3e7f0819fc2d6da66aa3d5d42c59e2e826f56
Johannes <Johannes@JO>
2017-05-22 22:11:20 +0200

    added method save() to interface ISetting()

--- src/Common/Settings/Setting.ts
+++ src/Common/Settings/Setting.ts
@@ -3,5 +3,6 @@ import { ISettingName } from "./SettingName";
 export interface ISetting<T> {
   key(): ISettingName;
   value(): Promise<T>;
+  save(value: T): Promise<void>;
   update(updateFunction: (value: T) => T): Promise<void>;
 }
