b1e5444ee04d77cbcc6eae85a5fd6ed22c53819e
Johannes <Johannes@JO>
2017-05-27 22:14:41 +0200

    removed commented source lines

--- src/ContentScript/TransferTableUi.ts
+++ src/ContentScript/TransferTableUi.ts
@@ -1,10 +1,6 @@
-//import { IRegisteredLoggingModule } from '../Common/Logger/RegisteredLoggingModule';
-//import { RegisteredLoggingModule } from '../Common/Logger/RegisteredLoggingModule';
-//import { LogLevelError } from '../Common/Logger/LogLevel';
 import { IStrengthLevelsSetting } from "../Common/Settings/StrengthLevelsSetting"
 import { ISetting } from "../Common/Toolkit/Setting";
 import { ITransferTablePossibleOffers } from "../Common/TransferTablePossibleOffers";
-//import { ILogger } from "../Common/Logger/Logger";
 import { IWebPageToExtend } from "../Common/Toolkit/WebPageToExtend";
 import { IUrl } from "../Common/Toolkit/Url";
 import { IDom } from "../Common/Toolkit/Dom";
@@ -14,7 +10,6 @@ export class TransferTableUi implements IWebPageToExtend {
   private domField: IDom;
   private urlField: IUrl;
   private log: IEasyLogger;
-//  private loggingModule: IRegisteredLoggingModule;
   private strengthLevelsSetting: IStrengthLevelsSetting;
   private transferTablePossibleOffers2: ISetting<ITransferTablePossibleOffers>;
 
@@ -27,8 +22,6 @@ export class TransferTableUi implements IWebPageToExtend {
   ) {
     this.domField = dom;
     this.urlField = webPageUrl;
-    //    this.loggingModule = new RegisteredLoggingModule("TransferTableUi", new LogLevelError());
-    //    this.log.registerModuleForLogging(this.loggingModule);
     this.strengthLevelsSetting = strengthLevelsSetting;
     this.transferTablePossibleOffers2 = transferTablePossibleOffersSetting2;
     this.log = logger;
@@ -42,12 +35,7 @@ export class TransferTableUi implements IWebPageToExtend {
   public addAdditionalInformation(doc: Document) {
     this.transferTablePossibleOffers2.value()
       .then(setting => {
-        //        var executeOnThisPage = doc.location.href.match("transfer/angebote.php") !== null;
-        //       this.log.info("called from: " + doc.location.href + ": " + executeOnThisPage);
-        if (
-          //        executeOnThisPage && 
-          setting.awpAndStrengthColumn().additionalInformationActivated()
-        ) {
+        if (setting.awpAndStrengthColumn().additionalInformationActivated()) {
           this.strengthLevelsSetting
             .strengthLevels()
             .then(strengthLevels => {
