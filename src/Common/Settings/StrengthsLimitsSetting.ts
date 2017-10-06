50a8b727ce278b2dc91c9a33e23bd8c52460b0e3
Johannes <Johannes@JO>
2017-10-06 21:43:18 +0200

    [added] implementation of changeStrengthLimits() function of (I)StrengthsLimitsSetting

--- src/Common/Settings/StrengthsLimitsSetting.ts
+++ src/Common/Settings/StrengthsLimitsSetting.ts
@@ -71,6 +71,13 @@ export class StrengthsLimitsSetting implements IStrengthsLimitsSetting {
   public changeStrengthsLimits(strengthLevelsLimits: Array<IStrengthLimits>): void {
   }
 
-  public changeStrengthLimits(strengthLevelLimits: IStrengthLimits): void {
+  public changeStrengthLimits(strengthLimits: IStrengthLimits): void {
+    this.strengthsLimits()
+      .then(strengthsLimits => {
+        this.strengthsLimitsSetting.update(strengthsLimits => {
+          strengthsLimits.update(strengthLimits);
+          return strengthsLimits;
+        });
+      });
   }
 }
