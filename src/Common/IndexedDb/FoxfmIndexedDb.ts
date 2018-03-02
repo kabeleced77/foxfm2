724931546934b406ab5b5d6f44ac8c8a853fb2e9
Kremp <jkremp@psi.de>
2018-03-02 14:45:21 +0100

    [formating] 2 space indent instead of 4 space indent

--- src/Common/IndexedDb/FoxfmIndexedDb.ts
+++ src/Common/IndexedDb/FoxfmIndexedDb.ts
@@ -2,12 +2,12 @@ import Dexie from "dexie";
 import { IMatchdayDataModel } from "../DataModel/MatchdayDataModel";
 
 export class FoxfmIndexedDb extends Dexie {
-    public matchdays: Dexie.Table<IMatchdayDataModel, Number>;
+  public matchdays: Dexie.Table<IMatchdayDataModel, Number>;
 
-    constructor() {
-        super("foxfm");
-        this.version(1).stores({
-            matchdays: "++id, &[serverValue+seasonValue+dayValue], serverValue, seasonValue, dayValue"
-        });
-    }
+  constructor() {
+    super("foxfm");
+    this.version(1).stores({
+      matchdays: "++id, &[serverValue+seasonValue+dayValue], serverValue, seasonValue, dayValue"
+    });
+  }
 }
--- src/Common/Matchday.ts
+++ src/Common/Matchday.ts
@@ -2,41 +2,41 @@ import { IMatchdayDataModel } from "./DataModel/MatchdayDataModel";
 import { FoxfmIndexedDb } from "./IndexedDb/FoxfmIndexedDb";
 
 export interface IMatchday {
-    id(): Number;
-    server(): Promise<String>;
-    day(): Promise<Number>;
-    season(): Number;
-    date(): Date;
+  id(): Number;
+  server(): Promise<String>;
+  day(): Promise<Number>;
+  season(): Number;
+  date(): Date;
 }
 
 export class Matchday implements IMatchday {
-    private source: FoxfmIndexedDb;
-    private idValue: Number;
+  private source: FoxfmIndexedDb;
+  private idValue: Number;
 
-    constructor(db: FoxfmIndexedDb, id: Number) {
-        this.source = db;
-        this.idValue = id;
-    }
+  constructor(db: FoxfmIndexedDb, id: Number) {
+    this.source = db;
+    this.idValue = id;
+  }
 
-    public id(): Number {
-        return this.idValue;
-    }
-    public server(): Promise<String> {
-        return this.source
-            .matchdays
-            .get(this.idValue)
-            .then((result: IMatchdayDataModel) => result.serverValue);
-    }
-    public day(): Promise<Number> {
-        return this.source
-            .matchdays
-            .get(this.idValue)
-            .then((result: IMatchdayDataModel) => result.dayValue);
-    }
-    public season(): Number {
-        throw new Error("Method not implemented.");
-    }
-    public date(): Date {
-        throw new Error("Method not implemented.");
-    }
+  public id(): Number {
+    return this.idValue;
+  }
+  public server(): Promise<String> {
+    return this.source
+      .matchdays
+      .get(this.idValue)
+      .then((result: IMatchdayDataModel) => result.serverValue);
+  }
+  public day(): Promise<Number> {
+    return this.source
+      .matchdays
+      .get(this.idValue)
+      .then((result: IMatchdayDataModel) => result.dayValue);
+  }
+  public season(): Number {
+    throw new Error("Method not implemented.");
+  }
+  public date(): Date {
+    throw new Error("Method not implemented.");
+  }
 }
