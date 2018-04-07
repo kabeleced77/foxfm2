d31fd515ae2bcf43ec200a69ca1cd6da77a343b0
Kremp <jkremp@psi.de>
2018-04-07 22:11:00 +0200

    [removed] interface IXPathSingleResult2 and implementataions [removed] interface IXPathHtmlTableCell2 and implementataions [removed] file ExperienceAndTrainingColumn.ts

--- src/Common/Toolkit/XPathSingleResult.ts
+++ src/Common/Toolkit/XPathSingleResult.ts
@@ -1,59 +1,9 @@
-import { IXPathAllResults } from './XPathAllResults'
-import { XPathAllResults, IXPathAllResults2, IXPathAllResults3 } from './XPathAllResults'
-import { IXPathString } from "./XPathString";
-
-export interface IXPathSingleResult2<T> {
-  element(doc: Document): T;
-  fromJson(jsonString: String): IXPathSingleResult2<T>;
-}
+import { IXPathAllResults } from './XPathAllResults';
+import { IXPathString } from './XPathString';
 
 export interface IXPathSingleResult<T> {
   element(): T;
   xPath(): IXPathString;
-}
-
-export class XPathSingleResult3<T extends Node> implements IXPathSingleResult2<T> {
-  private xPathAllResults: IXPathAllResults3;
-
-  constructor(xPathAllResults: IXPathAllResults3) {
-    this.xPathAllResults = xPathAllResults;
-  }
-
-  public element(doc: Document): T {
-    if (this.xPathAllResults.xPathNumberOfResults(doc) === 1) {
-      return <T>this.xPathAllResults.xPathFirstResult(doc);
-    }
-    else {
-      throw new Error(`XPath provided no or more than a single result: ${this.xPathAllResults.xPath().xPathString()} -> results: ${this.xPathAllResults.xPathNumberOfResults(doc)}`);
-    }
-  }
-  public fromJson(jsonString: String): IXPathSingleResult2<T> {
-    return new XPathSingleResult3<T>(
-      this.xPathAllResults.fromJson(jsonString["xPathAllResults"])
-    );
-  }
-}
-
-export class XPathSingleResult2<T extends Node> implements IXPathSingleResult2<T> {
-  private xPathAllResults: IXPathAllResults2;
-
-  constructor(xPathAllResults: IXPathAllResults2) {
-    this.xPathAllResults = xPathAllResults;
-  }
-
-  public element(doc: Document): T {
-    if (this.xPathAllResults.xPathNumberOfResults(doc) === 1) {
-      return <T>this.xPathAllResults.xPathFirstResult(doc);
-    }
-    else {
-      throw new Error(`XPath provided no or more than a single result: ${this.xPathAllResults.xPath().xPathString()} -> results: ${this.xPathAllResults.xPathNumberOfResults(doc)}`);
-    }
-  }
-  public fromJson(jsonString: String): IXPathSingleResult2<T> {
-    return new XPathSingleResult2<T>(
-      this.xPathAllResults.fromJson(jsonString["xPathAllResults"])
-    );
-  }
 }
 
 export class XPathSingleResult<T extends Node> implements IXPathSingleResult<T> {
