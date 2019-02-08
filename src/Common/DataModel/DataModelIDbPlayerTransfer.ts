485bc936b38ee0958a288486d291e3cd70ab213a
Kremp <jkremp@psi.de>
2019-02-08 14:05:15 +0100

    [refactored] DataModelIDbPlayerTransfer(): public properties declared in ctor

--- src/Common/DataModel/DataModelIDbPlayerTransfer.ts
+++ src/Common/DataModel/DataModelIDbPlayerTransfer.ts
@@ -11,29 +11,14 @@ export interface IDataModelIDbPlayerTransfer {
 
 export class DataModelIDbPlayerTransfer implements IDataModelIDbPlayerTransfer {
   public id: Number;
-  public gameServerId: Number;
-  public matchdayId: Number;
-  public externalTransferId: Number;
-  public position: String;
-  public age: Number;
-  public strength: Number;
-  public price: Number;
 
   constructor(
-    gameServerId: Number,
-    matchdayId: Number,
-    externalTransferId: Number,
-    position: String,
-    age: Number,
-    strength: Number,
-    price: Number,
-  ) {
-    this.gameServerId = gameServerId;
-    this.matchdayId = matchdayId;
-    this.externalTransferId = externalTransferId;
-    this.position = position;
-    this.age = age;
-    this.strength = strength;
-    this.price = price;
-  }
+    public gameServerId: Number,
+    public matchdayId: Number,
+    public externalTransferId: Number,
+    public position: String,
+    public age: Number,
+    public strength: Number,
+    public price: Number,
+  ) { }
 }
