8e768b97efdfbe8386b156f9fede9fac54bd6ebf
kabeleced <kabeleced@arcor.de>
2019-04-20 23:17:29 +0200

    [refactored] HtmlTableColumnStringValues.values(): use TEXT_NODE values of first child of table data cells instead of cell'sinnerHTML

--- src/Common/Toolkit/HtmlTableColumnStringValues.ts
+++ src/Common/Toolkit/HtmlTableColumnStringValues.ts
@@ -11,6 +11,16 @@ export class HtmlTableColumnStringValues implements IValues<String> {
   }
 
   public values(): String[] {
-    return this.columnValues.values().map(element => element.innerHTML);
+    return this.columnValues
+      .values()
+      .map((element: HTMLTableDataCellElement) => this.firstInnerMost(element));
+  }
+
+  private firstInnerMost(element: Node): String {
+    if (element && element.nodeType != Node.TEXT_NODE && element.hasChildNodes()) {
+      return this.firstInnerMost(element.firstChild!);
+    } else {
+      return element.textContent!;
+    }
   }
 }
