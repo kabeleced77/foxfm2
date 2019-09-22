6cafe7f980ec8f8efd3506175a6a10905f83783a
kabeleced <kabeleced@arcor.de>
2019-09-22 20:43:36 +0200

    [extended] [I]GameServersIDb(): added method gameServersByUri()

--- src/Common/IGameServers.ts
+++ src/Common/IGameServers.ts
@@ -1,6 +1,7 @@
 import { IGameServerWithId } from "./IGameServerWithId";
 
 export interface IGameServers {
+  gameServersByUri(uri: String): Promise<IGameServerWithId[]>;
   gameServers(): Promise<IGameServerWithId[]>;
   add(uri: String): Promise<void | IGameServerWithId>;
 }
--- src/Common/IndexedDb/GameServersIDb.ts
+++ src/Common/IndexedDb/GameServersIDb.ts
@@ -1,16 +1,32 @@
-import { DataModelIDbGameServer } from '../DataModel/DataModelIDbGameServer';
+import { DataModelIDbGameServer, IDataModelIDbGameServer } from '../DataModel/DataModelIDbGameServer';
 import { GameServerIDb } from './GameServerIDb';
 import { IGameServerWithId } from "../IGameServerWithId";
 import { FoxfmIndexedDb } from './FoxfmIndexedDb';
 import { IGameServers } from '../IGameServers';
+import { IEasyLogger } from '../Logger/EasyLogger';
 
 export class GameServersIDb implements IGameServers {
   private dataBase: FoxfmIndexedDb;
 
   constructor(
     source: FoxfmIndexedDb,
+    private logger: IEasyLogger,
   ) {
     this.dataBase = source;
+  }
+
+  public async gameServersByUri(
+    uri: String,
+  ): Promise<IGameServerWithId[]> {
+    this.logger.debug(`will search for game servers by uri '${uri}'`);
+    let gameServers: IGameServerWithId[] = [];
+    await this.dataBase
+      .gameServers
+      .where(`${nameof<IDataModelIDbGameServer>(o => o.uri)}`)
+      .equals(uri.toString())
+      .eachPrimaryKey((pk: Number) => gameServers.push(new GameServerIDb(this.dataBase, pk)));
+    this.logger.debug(`number of found game servers by uri '${uri}': ${gameServers.length}`);
+    return gameServers;
   }
 
   public gameServers(): Promise<IGameServerWithId[]> {
