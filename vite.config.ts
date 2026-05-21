ccdb2bc0e2535eae8deafc3a38359c9cbc4996e4
kabeleced <kabeleced@arcor.de>
2026-05-21 21:27:12 +0200

    refactor: add vite-plugin-zip-pack for packaging builds and update build scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+import zipPack from "vite-plugin-zip-pack";
 import mergeJsonPlugin from "./vite-plugins/mergeJsonPlugin";
 
 function generateManifest() {
@@ -16,16 +17,23 @@ function generateManifest() {
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: { emptyOutDir: true },
   plugins: [
     react(),
     webExtension({
       manifest: generateManifest,
+      browser: process.env.TARGET || "chrome",
     }),
     mergeJsonPlugin({
       sourceFolder: "_locales", // Example: your locales folder
-      outputFolder: "dist/_locales",    // Example: output folder
-      outputFileName: "messages.json",   // Example: output file name
+      outputFolder: "dist/_locales", // Example: output folder
+      outputFileName: "messages.json", // Example: output file name
       suffixSeparator: "-", // Optional: separator for suffixes in file names
+    }),
+    zipPack({
+      inDir: "dist",
+      outDir: ".",
+      outFileName: `foxfm-${process.env.TARGET}.${process.env.TARGET === "firefox" ? "xpi" : "zip"}`,
     }),
   ],
 });
